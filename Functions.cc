ef62115f8c712914c7e41686d8caab968c9d41bf
acarvalh <alexandracaan@gmail.com>
2014-01-13 18:42:28 -0200

    Create Functions.cc

---
+++ Functions.cc
@@ -0,0 +1,27 @@
+#include <fstream>
+#include "fastjet/ClusterSequence.hh"
+#include "fastjet/tools/Filter.hh"
+#include "fastjet/Selector.hh"
+#include "fastjet/tools/MassDropTagger.hh"
+#include "deltaphi.h"
+#include <TCanvas.h>
+#include <TFile.h>
+#include <TArray.h>
+#include <TH1F.h>
+#include <TH1D.h>
+#include <TLegend.h>
+#include <TArray.h>
+#include <TVector.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <TString.h>
+#include <TObject.h>
+#include <TMath.h>
+
+using namespace fastjet;
+using namespace std;
+
+void hello(){
+  cout<<"\n\n\n HELLO!!!! \n\n"<<endl;
+}

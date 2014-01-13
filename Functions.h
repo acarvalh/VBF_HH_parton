34cd6571306fd2de59b285799e2c01ca7c0ec544
acarvalh <alexandracaan@gmail.com>
2014-01-13 18:42:01 -0200

    Create Functions.h

---
+++ Functions.h
@@ -0,0 +1,8 @@
+#include "fastjet/ClusterSequence.hh"
+#include "fastjet/Selector.hh"
+#include "fastjet/tools/Filter.hh"
+#include "fastjet/tools/MassDropTagger.hh"
+#include <TH1F.h>
+
+// Test function
+void hello();

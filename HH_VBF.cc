bcf0f7090864a58080490d692f73fdb494504032
Xanda <xanda@xanda-ThinkPad-X220-Tablet.(none)>
2014-01-15 03:44:40 -0200

    btag fixed

--- HH_VBF.cc
+++ HH_VBF.cc
@@ -34,8 +34,8 @@ int main() {
     in1.open(filename.at(i).c_str());
     // declare the root plots to be done
     decla(mass[i]);
-//    for(unsigned ievent=0;ievent<nevent;ievent++){ // for each event  // 
-    for(unsigned ievent=0;ievent<20;ievent++){ // for each event  // 
+    for(unsigned ievent=0;ievent<nevent;ievent++){ // for each event  // 
+//    for(unsigned ievent=0;ievent<20;ievent++){ // for each event  // 
         cout<<"----------------------------------------------------"<<endl;
         string c;
         in1>>c;
@@ -48,23 +48,26 @@ int main() {
         vector<PseudoJet> photons;                    
         int nb = 0;
         int nvbf = 0;
-        in1>>nparticles;  
+        in1>>nparticles;  int counter=0; 
         for(unsigned ipart=0;ipart<nparticles;ipart++){ // loop on particles
           in1 >> pID >> Px >> Py >> Pz >> E ;//>> idup;
-          //cout<< pID <<endl;    
+          //cout<< pID <<endl;   
           if (abs(pID) < 6 ){  // if a quark/gluon -- neglect hadrons
-		particles.push_back(fastjet::PseudoJet(Px,Py,Pz,E)); particles.at(nvbf).set_user_index(pID); 
+		particles.push_back(fastjet::PseudoJet(Px,Py,Pz,E)); particles.at(counter).set_user_index(pID); 
 		if(abs(pID) == 5) nb++; else nvbf++; // count b's and no-b's
+                //cout<<"particle flavour "<< particles.at(counter).user_index()<<endl;
+		counter++;
 	  }           
         } // finish to read all particles
         // construct the jets
-        cout<<"nparticles = "<< nb+nvbf<<endl;
+        //cout<<"number of b particles = "<< nb<<endl;
+        //cout<<"number of non-b particles = "<< nvbf<<endl;
 	vector<PseudoJet> jets; 
         // check tags
 	vector<int> btag; vector<int> bmistag; // number of btags/bmistags of each jet entry 
 	vector<int> fattag;
         int njets = recojets(particles, jets,btag,bmistag);
-	cout<<njets<<endl; 
+	cout<<"njets "<<njets<<endl; 
         istagged(jets, fattag); // check wheather the jet is fat -- by now only invariant mass
 	// VBF combinatorics and cuts
 	vector<int> vbftag; // keeps the entries of jets that are the VBF, have two entries

660b99d1f27584f1ebdd77840cc6dba002457139
Xanda <xanda@xanda-ThinkPad-X220-Tablet.(none)>
2014-01-15 03:45:16 -0200

    loose

--- cuts.h
+++ cuts.h
@@ -4,8 +4,8 @@ double HTVBF = 0;//400;
 double DeltayVBF = 0;//3;
 bool shower=false;
 // To be applied only to hadron level events
-double const jet_ptmin=30.0; // We cut on all jets below 50 GeV
-double const rapmax=2.5;
+double const jet_ptmin=20.0; // We cut on all jets below 50 GeV
+double const rapmax=5;
 double const HTmin = 0;
 double const higgs_mass = 125.0;
 // on the 4b's analysis

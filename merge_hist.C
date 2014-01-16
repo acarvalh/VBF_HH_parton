0cfac8633062228dee870ef5c3b9d17a8d8ed2f5
Xanda <xanda@xanda-ThinkPad-X220-Tablet.(none)>
2014-01-16 00:33:00 -0200

    working

--- merge_hist.C
+++ merge_hist.C
@@ -62,47 +62,30 @@ TStyle *defaultStyle = new TStyle("defaultStyle","Default Style");
     defaultStyle->cd();
 /////////////////////////////////////////////////////
 
-int nmass = 24;
+int nmass = 11;
 //int mass[nmass]= {300,500,600,700,800,900,1500,2500,3000};
 
 const char* channel[nmass]={
 "histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-//
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-//
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root",
-"histos/Control_shower_260.root"
+"histos/Control_shower_0.root",
+"histos/Control_shower_1.root",
+"histos/Control_shower_137.root",
+"histos/Control_shower_450.root",
+"histos/Control_shower_500.root",
+"histos/Control_shower_550.root",
+"histos/Control_shower_600.root",
+"histos/Control_shower_650.root",
+"histos/Control_shower_700.root",
+"histos/Control_shower_750.root",
 };
 
 //const char* lege[nmass]={"300 GeV","500 GeV","600 GeV","700 GeV","800 GeV","900 GeV","1500 GeV","2500 GeV","3000 GeV"};
 
-const char* lege[nmass]={"250 GeV","300 GeV","350 GeV","400 GeV","450 GeV","500 GeV","550 GeV","600 GeV","650 GeV","700 GeV","750 GeV","800 GeV","850 GeV","900 GeV","950 GeV","1000 GeV","1100 GeV","1200 GeV","1300 GeV","1400 GeV","1500 GeV","2000 GeV","2500 GeV","3000 GeV"};
+//const char* lege[nmass]={"260 GeV","300 GeV","350 GeV","400 GeV","450 GeV","500 GeV","550 GeV","600 GeV","650 GeV","700 GeV","750 GeV"};
+const char* lege[nmass]={"260 GeV","cg =0","cg=1","cg=0.0137","450 GeV","500 GeV","550 GeV","600 GeV","650 GeV","700 GeV","750 GeV"};
 //for (int k=0; k<nmass; k++) channel[k] = Form("Control_shower_%d.root", k);
 
-
-int nplots =25;
-
- TLegend *leg = new TLegend(0.65,0.250,0.95,0.90);
+ TLegend *leg = new TLegend(0.75,0.550,0.99,0.99);
    leg->SetTextSize(0.03146853);
    leg->SetLineColor(1);
    leg->SetLineStyle(1);
@@ -111,52 +94,72 @@ int nplots =25;
 
  leg->SetHeader("BKG to 8TeV");
 
-
-   const char* namplots[nplots] = {
-        "DiPhotonMass.png","DiPhotonE.png", "DiPhotonPt.png", "DiPhotonEta.png", "DiPhotonPhi.png", 
-	"njets.png", //1
-	"DijetMass.png","DijetE.png","DijetPt.png","DijetEta.png","DijetPhi.png", // 5
-	"radMass.png","radE.png","radPt.png","radEta.png","radPhi.png", // 5
-	"DRphph.png",  //DPhiphph, // 2
-	"DRhh.png", //DPhihh, // 2
-	"DRjj.png",// DPhijj, // 2
-	"empty.png","empty2.png","lead.png","sublead.png","fattagreco.png","fattagrecon.png"
-	//0,fabs(Higgs_ph.rapidity()-Dijet.rapidity())//ph1_pt/ph2_pt,jpt[nlead]/jpt[nsublead] // 2
+ int nplots =25;
+ const char* namplots[nplots] = {
+           "njets_passing_kLooseID_ct4.png",
+           "H1hist.png",
+           "H1histpt.png",
+           "H1histeta.png",
+           "H1histphi.png",
+           "H2hist.png",
+           "H2histpt.png",
+           "H2histeta.png",
+           "H2histphi.png",
+           "RadMass_ct4.png",
+           "radion_pt_ct4.png",
+           "radion_eta_ct4.png",
+           "radion_phi_ct4.png",  
+           "fattag_ct4.png",
+           "btag_ct4.png",
+           "bmistag_ct4.png",
+           "j1histpt.png",
+           "j1histeta.png",
+           "j2histpt.png",
+           "j2histeta.png",
+           "RadMass_ct4vbf.png",
+           "Deta.png",
+           "fattag_ct4vbf.png",
+           "btag_ct4vbf.png",
+           "bmistag_ct4vbf.png"
     }; //25
-
 
 TH1D* plots[nmass][nplots];//[file][plot]
 TFile *file[nmass];
 for(int i=0;i<nmass;i++){
  TFile *file[i] = TFile::Open(channel[i]);
- TH1D* plots[i][0] = (TH1D* ) file[i]->Get("PhotonsMass_ct4;1");//
- TH1D* plots[i][1] = (TH1D* ) file[i]->Get("dipho_E_ct4;1");
- TH1D* plots[i][2] = (TH1D* ) file[i]->Get("dipho_pt_ct4;1");
- TH1D* plots[i][3] = (TH1D* ) file[i]->Get("dipho_eta_ct4;1");
- TH1D* plots[i][4] = (TH1D* ) file[i]->Get("dipho_phi_ct4;1");
- TH1D* plots[i][5] = (TH1D* ) file[i]->Get("njets_passing_kLooseID_ct4;1");
- TH1D* plots[i][6] = (TH1D* ) file[i]->Get("JetsMass_ct4;1");
- TH1D* plots[i][7] = (TH1D* ) file[i]->Get("dijet_E_ct4;1");
- TH1D* plots[i][8] = (TH1D* ) file[i]->Get("dijet_pt_ct4;1");
- TH1D* plots[i][9] = (TH1D* ) file[i]->Get("dijet_eta_ct4;1");
- TH1D* plots[i][10] = (TH1D* ) file[i]->Get("dijet_phi_ct4;1");
- TH1D* plots[i][11] = (TH1D* ) file[i]->Get("RadMass_ct4;1");
- TH1D* plots[i][12] = (TH1D* ) file[i]->Get("radion_E_ct4;1");
- TH1D* plots[i][13] = (TH1D* ) file[i]->Get("radion_pt_ct4;1");
- TH1D* plots[i][14] = (TH1D* ) file[i]->Get("radion_eta_ct4;1");
- TH1D* plots[i][15] = (TH1D* ) file[i]->Get("radion_phi_ct4;1");
- TH1D* plots[i][16] = (TH1D* ) file[i]->Get("DRphph_ct4;1");
- TH1D* plots[i][17] = (TH1D* ) file[i]->Get("DRhh_ct4;1");
- TH1D* plots[i][18] = (TH1D* ) file[i]->Get("DRjj_ct4;1");
- TH1D* plots[i][19] = (TH1D* ) file[i]->Get("aaass_ct4;1");
- TH1D* plots[i][20] = (TH1D* ) file[i]->Get("jjass_ct4;1");
- TH1D* plots[i][21] = (TH1D* ) file[i]->Get("leadsel_ct4;1");
- TH1D* plots[i][22] = (TH1D* ) file[i]->Get("sublead_ct4;1");
- TH1D* plots[i][23] = (TH1D* ) file[i]->Get("fattagreco_ct4;1");
- TH1D* plots[i][24] = (TH1D* ) file[i]->Get("fattagrecon_ct4;1");
+ TH1D* plots[i][0] = (TH1D* ) file[i]->Get("njets_passing_kLooseID_ct4;1"); 
+ TH1D* plots[i][1] = (TH1D* ) file[i]->Get("H1hist;1"); 
+ TH1D* plots[i][2] = (TH1D* ) file[i]->Get("H1histpt;1"); 
+ TH1D* plots[i][3] = (TH1D* ) file[i]->Get("H1histeta;1"); 
+ TH1D* plots[i][4] = (TH1D* ) file[i]->Get("H1histphi;1");
+ TH1D* plots[i][5] = (TH1D* ) file[i]->Get("H2hist;1"); 
+ TH1D* plots[i][6] = (TH1D* ) file[i]->Get("H2histpt;1"); 
+ TH1D* plots[i][7] = (TH1D* ) file[i]->Get("H2histeta;1"); 
+ TH1D* plots[i][8] = (TH1D* ) file[i]->Get("H2histphi;1"); 
+ TH1D* plots[i][9] = (TH1D* ) file[i]->Get("RadMass_ct4;1"); 
+ TH1D* plots[i][10] = (TH1D* ) file[i]->Get("radion_pt_ct4;1"); 
+ TH1D* plots[i][11] = (TH1D* ) file[i]->Get("radion_eta_ct4;1"); 
+ TH1D* plots[i][12] = (TH1D* ) file[i]->Get("radion_phi_ct4;1");
+ TH1D* plots[i][13] = (TH1D* ) file[i]->Get("fattag_ct4;1"); 
+ TH1D* plots[i][14] = (TH1D* ) file[i]->Get("btag_ct4;1"); 
+ TH1D* plots[i][15] = (TH1D* ) file[i]->Get("bmistag_ct4;1"); 
+ TH1D* plots[i][16] = (TH1D* ) file[i]->Get("j1histpt;1"); 
+ TH1D* plots[i][17] = (TH1D* ) file[i]->Get("j1histeta;1"); 
+ TH1D* plots[i][18] = (TH1D* ) file[i]->Get("j1histeta;1");//"j2histpt;1"); 
+ TH1D* plots[i][19] = (TH1D* ) file[i]->Get("j1histeta;1");//"j2histeta;1"); 
+ TH1D* plots[i][20] = (TH1D* ) file[i]->Get("RadMass_ct4vbf;1"); 
+ TH1D* plots[i][21] = (TH1D* ) file[i]->Get("Deta;1"); 
+ TH1D* plots[i][22] = (TH1D* ) file[i]->Get("fattag_ct4vbf;1"); 
+ TH1D* plots[i][23] = (TH1D* ) file[i]->Get("btag_ct4vbf;1"); 
+ TH1D* plots[i][24] = (TH1D* ) file[i]->Get("bmistag_ct4vbf;1");
 } 
 
-const int sigcolor[nmass]={2,3,5,6,7,8,9,11,12,2,3,5,6,7,8,9,11,12,2,3,5,6,7,8};
+const int sigcolor[nmass]={
+	2,3,5,6,7,
+	8,9,11,12,
+	50,227};//,5,6,7,
+	//8,9,11,12,
+	//2,3,5,6,5,8};
 for(int k=0;k<nplots;k++) for(int l=0;l<nmass;l++){
 //plots[0][i]->SetFillColor(kCyan+1);
 //plots[1][i]->SetFillColor(kCyan+2);// terra qcd_30_8TeV_ff.root
@@ -166,23 +169,27 @@ plots[l][k]->SetLineStyle(0);
 plots[l][k]->SetLineWidth(3);
 
 //
-//cout<<"here"<<endl;
+cout<<"here "<<k<<" "<<l<<endl;
 }
-
+cout<<"here"<<endl;
 TCanvas* PT_HAT = new TCanvas();
 PT_HAT->cd(); 
-
+int max=4;//nmass;
+double nevents[4]={20000,50000,50000,50000};
 for(int i=0;i<nplots;i++) {
 //if(i==16 || i==4 || i==5 || i==6 || i==7  || i==12) PT_HAT->SetLogy(1); else PT_HAT->SetLogy(0);
-
-	for(int j=0;j<nmass;j++) leg->AddEntry(plots[j][i],lege[j],"l");
+	
+	for(int j=0;j<max;j++) leg->AddEntry(plots[j][i],lege[j],"l");
 	//plots[0][i].Scale(plots[0][i].Integral());
-	plots[0][i].SetMaximum(4*plots[0][i].GetMaximum());
+        //plots[0][i].Scale(nevents[j]);
+	plots[0][i].SetMaximum(1.2*plots[0][i].GetMaximum());
 	plots[0][i].Draw("Hist");
 	leg->Draw("same");
-	for(int j=1;j<nmass;j++) {
-	//	plots[j][i].Scale(plots[j][i].Integral());
+	for(int j=1;j<max;j++) {
+        	//plots[j][i].Scale(nevents[j]);
+		//plots[j][i].Scale(plots[j][i].Integral());
 		plots[j][i].Draw("Hist,same");
+	
 	}
 	
 	PT_HAT->SaveAs(namplots[i]);

5333237a739fe52c05a8cce2ce485da62b9a650e
Xanda <xanda@xanda-ThinkPad-X220-Tablet.(none)>
2014-01-17 15:07:32 -0200

    names convetions

--- HH_VBF.cc
+++ HH_VBF.cc
@@ -18,25 +18,30 @@ int main() {
   // input
   vector<string> filename;
   string file, path,data;
-  path="bulk_graviton/kkgraviton_cg";
+  int points;
+  if(resonant) {path="bulk_graviton/Madgraph0/MGraviton_"; points = masses;}
+  else {path="nonresonant/pp_hh_vbf_"; points = parameters;}
   data = ".lhe.decayed";
-  for(unsigned i=0;i<masses;i++){
-    ostringstream o;
-    o << "" << mass[i];
-    file = path + o.str() + data;
+  
+  for(unsigned i=0;i<points;i++){
+    if(resonant) {
+      ostringstream o;
+      o << "" << mass[i];//filenames
+      file = path + o.str() + data;
+    } else file = path + filenames[i] + data;
     filename.push_back(file);
-    cout<<filename.at(i)<<endl;
+    cout<<" "<<points<<" "<<i<<" "<<filename.at(i)<<endl;
   } // close filename
   //////////////////////////////////////////////////////////////////////////////////////
   ifstream in1;
-  for(int i=0;i<masses;i++){ // for each mass
+  // declare the root plots to be done
+  decla(0);
+  for(int i=0;i<points;i++){ // for each mass
     cout<<"\n\n reading file = "<<filename.at(i)<<endl;
     in1.open(filename.at(i).c_str());
-    // declare the root plots to be done
-    decla(mass[i]);
     for(unsigned ievent=0;ievent<nevent;ievent++){ // for each event  // 
-//    for(unsigned ievent=0;ievent<20;ievent++){ // for each event  // 
-        cout<<"----------------------------------------------------"<<endl;
+//    for(unsigned ievent=0;ievent<2;ievent++){ // for each event  // 
+        //cout<<"----------------------------------------------------"<<endl;
         string c;
         in1>>c;
         double Px, Py , Pz, E;
@@ -53,7 +58,8 @@ int main() {
           in1 >> pID >> Px >> Py >> Pz >> E ;//>> idup;
           //cout<< pID <<endl;   
           if (abs(pID) < 6 ){  // if a quark/gluon -- neglect hadrons
-		particles.push_back(fastjet::PseudoJet(Px,Py,Pz,E)); particles.at(counter).set_user_index(pID); 
+		particles.push_back(fastjet::PseudoJet(Px,Py,Pz,E)); 
+                particles.at(counter).set_user_index(pID); 
 		if(abs(pID) == 5) nb++; else nvbf++; // count b's and no-b's
                 //cout<<"particle flavour "<< particles.at(counter).user_index()<<endl;
 		counter++;
@@ -67,7 +73,7 @@ int main() {
 	vector<int> btag; vector<int> bmistag; // number of btags/bmistags of each jet entry 
 	vector<int> fattag;
         int njets = recojets(particles, jets,btag,bmistag);
-	cout<<"njets "<<njets<<endl; 
+	//cout<<"njets "<<njets<<endl; 
         istagged(jets, fattag); // check wheather the jet is fat -- by now only invariant mass
 	// VBF combinatorics and cuts
 	vector<int> vbftag; // keeps the entries of jets that are the VBF, have two entries
@@ -79,7 +85,7 @@ int main() {
 	  if(fourb) reco = analyse_4b(jets,fattag,btag,bmistag,vbftag);
         } // close pass VBF
     } // close for event
-    save_hist(mass[i]);
+    if(resonant) save_hist(mass[i],resonant); else save_hist(parameter[i],resonant);
+    in1.close();
   } // close for each mass
-
 }

30362dc0b6a3e42cb98ad1740f2c90851d50ddac
Xanda <xanda@xanda-ThinkPad-X220-Tablet.(none)>
2014-01-19 19:37:31 -0200

    more plots

--- merge_hist.C
+++ merge_hist.C
@@ -79,10 +79,6 @@ const char* channel[nmass]={
 "histosnonres/Control_shower_101000.root",
 "histosnonres/Control_shower_101020.root"
 };
-int maxtodo=3; 
-int todo[maxtodo]={0,1,2};
-double masses[maxtodo] = {0,1,2};//,650,700,750};
-
 const char* lege[nmass]={
 "SM",
 "SM VBFcuts",
@@ -97,6 +93,13 @@ const char* lege[nmass]={
 "CV=1 C2V=1 C3=0",
 "CV=1 C2V=1 C3=2"
 };
+
+int maxtodo=7; 
+//int todo[maxtodo]={0,1,2};
+//int todo[maxtodo]={1,7,8};
+int todo[maxtodo]={2,3,4,5,6,7,8};
+//double masses[maxtodo] = {0,1,2};//,650,700,750};
+double masses[maxtodo] = {0,1,2,3,4,5,6};//,650,700,750};
 
  TLegend *leg = new TLegend(0.55,0.550,0.99,0.99);
    leg->SetTextSize(0.03146853);
@@ -114,7 +117,7 @@ const char* lege[nmass]={
 
  leg->SetHeader("BKG to 8TeV");
 
- int nplots =25;
+ int nplots =27;
  const char* namplots[nplots] = {
            "njets_passing_kLooseID_ct4.png",
            "H1hist.png",
@@ -140,13 +143,15 @@ const char* lege[nmass]={
            "Deta.png",
            "fattag_ct4vbf.png",
            "btag_ct4vbf.png",
-           "bmistag_ct4vbf.png"
+           "bmistag_ct4vbf.png",
+           "cat.png",
+           "detahh.png"
     }; //25
 
-TH1D* plots[nmass][nplots];//[file][plot]
-TFile *file[nmass];
-for(int i=0;i<nmass;i++){
- TFile *file[i] = TFile::Open(channel[i]);
+TH1D* plots[maxtodo][nplots];//[file][plot]
+TFile *file[maxtodo];
+for(int i=0;i<maxtodo;i++){
+ TFile *file[i] = TFile::Open(channel[todo[i]]);
  TH1D* plots[i][0] = (TH1D* ) file[i]->Get("njets_passing_kLooseID_ct4;1"); 
  TH1D* plots[i][1] = (TH1D* ) file[i]->Get("H1hist;1"); 
  TH1D* plots[i][2] = (TH1D* ) file[i]->Get("H1histpt;1"); 
@@ -164,7 +169,7 @@ for(int i=0;i<nmass;i++){
  TH1D* plots[i][14] = (TH1D* ) file[i]->Get("btag_ct4;1"); 
  TH1D* plots[i][15] = (TH1D* ) file[i]->Get("bmistag_ct4;1"); 
  TH1D* plots[i][16] = (TH1D* ) file[i]->Get("j1histpt;1"); 
- TH1D* plots[i][17] = (TH1D* ) file[i]->Get("j1histeta;1"); 
+ TH1D* plots[i][17] = (TH1D* ) file[i]->Get("j2histpt;1"); 
  TH1D* plots[i][18] = (TH1D* ) file[i]->Get("j1histeta;1");//"j2histpt;1"); 
  TH1D* plots[i][19] = (TH1D* ) file[i]->Get("j1histeta;1");//"j2histeta;1"); 
  TH1D* plots[i][20] = (TH1D* ) file[i]->Get("RadMass_ct4vbf;1"); 
@@ -172,6 +177,8 @@ for(int i=0;i<nmass;i++){
  TH1D* plots[i][22] = (TH1D* ) file[i]->Get("fattag_ct4vbf;1"); 
  TH1D* plots[i][23] = (TH1D* ) file[i]->Get("btag_ct4vbf;1"); 
  TH1D* plots[i][24] = (TH1D* ) file[i]->Get("bmistag_ct4vbf;1");
+ TH1D* plots[i][25] = (TH1D* ) file[i]->Get("cat_ct4;1");
+ TH1D* plots[i][26] = (TH1D* ) file[i]->Get("detahh_ct4;1");
 } 
 
 const int sigcolor[nmass]={
@@ -181,8 +188,8 @@ const int sigcolor[nmass]={
 	//8,9,11,12,
 	//2,3,5,6,5,8};
 
-for(int k=0;k<nplots;k++) for(int l=0;l<nmass;l++){
-plots[l][k]->SetLineColor(sigcolor[l]);
+for(int k=0;k<nplots;k++) for(int l=0;l<maxtodo;l++){
+plots[l][k]->SetLineColor(sigcolor[todo[l]]);
 plots[l][k]->SetLineStyle(0);
 plots[l][k]->SetLineWidth(3);
 cout<<"here "<<k<<" "<<l<<endl;
@@ -192,25 +199,25 @@ TCanvas* PT_HAT = new TCanvas();
 PT_HAT->cd(); 
 int max=nmass;
 //Double_t nevents[4]={20000.,20000.,20000.,20000.};//20000.,20000.,20000.,20000.};
-double high[nplots]={1.2,1.2,1.2,2.1,1.2,
-		     1.2,1.2,1.7,1.2,1.2,
+double high[nplots]={1.2,1.2,4.2,2.1,1.2,
+		     1.2,4.2,1.7,1.2,1.2,
 		     1.2,1.2,1.2,1.2,1.2,
 		     1.2,1.2,1.7,1.7,1.7,
-                     1.2,1.2,1.2,1.2,1.2}; 
+                     1.2,1.2,1.2,1.2,1.2,1.2,1.9}; 
 
   for(int i=0;i<nplots;i++) {
   //if(i==16 || i==4 || i==5 || i==6 || i==7  || i==12) PT_HAT->SetLogy(1); else PT_HAT->SetLogy(0);
-	
-	for(int j=0;j<maxtodo;j++) leg->AddEntry(plots[todo[j]][i],lege[todo[j]],"l");
+	for(int j=0;j<maxtodo;j++) leg->AddEntry(plots[j][i],lege[todo[j]],"l");
 	//plots[0][i].Scale(plots[0][i].Integral());
         //plots[0][i].Scale(nevents[j]);
-	plots[todo[0]][i].SetMaximum(high[i]*plots[todo[0]][i].GetMaximum());
-	plots[todo[0]][i].Draw("Hist");
+	plots[0][i].SetMaximum(high[i]*plots[todo[0]][i].GetMaximum());
+	plots[0][i].Draw("Hist");
 	leg->Draw("same");
 	for(int j=1;j<maxtodo;j++) {
+	//if(i==1) cout<<"higgs mass "<<plots[j][i].Integral() <<endl;
         //	plots[j][i].Scale(nevents[j]);
 		//plots[j][i].Scale(plots[j][i].Integral());
-		plots[todo[j]][i].Draw("Hist,same");
+		plots[j][i].Draw("Hist,same");
 	
 	}
 	
@@ -220,28 +227,37 @@ double high[nplots]={1.2,1.2,1.2,2.1,1.2,
   }
 //////////////////////////////////////////////////////////////////////
 // njets plot
-vector<double> nj3,nj4,nj5,nj6,nj7,nj8,nj9,nj10,njmore,ntot;
-    for(int j=0;j<maxtodo;j++) {
+
+vector<double> nj0,nj1,nj2,nj3,nj4,nj5,nj6,nj7,nj8,nj9,nj10,njmore,ntot;
+    for(int j=0;j<maxtodo;j++) { // to each sample
        cout<<lege[todo[j]]<<endl;
        // get number of njets
-       int nbins = plots[todo[j]][0].GetNbinsX();
+       int nbins = plots[j][0].GetNbinsX();
        //cout<<nbins<<endl;
-       for(int k=0;k<nbins;k++) {
-	double njets = plots[todo[j]][0].GetBinContent(k); 
-        if (k == 3)      {nj3.push_back(njets); cout<<"3 jets "<<nj3[j]<<endl;}
-        else if (k == 4) {nj4.push_back(njets); cout<<"4 jets "<<nj4[j]<<endl;}
-        else if (k == 5) {nj5.push_back(njets); cout<<"5 jets "<<nj5[j]<<endl;}
-        else if (k == 6) {nj6.push_back(njets); cout<<"6 jets "<<nj6[j]<<endl;}
-        else if (k == 7) {nj7.push_back(njets); cout<<"7 jets "<<nj7[j]<<endl;}
-        else if (k == 8) {nj8.push_back(njets); cout<<"8 jets "<<nj8[j]<<endl;}
-        else if (k == 9) {nj9.push_back(njets); cout<<"9 jets "<<nj9[j]<<endl;}
-        else if (k == 10) {nj10.push_back(njets); cout<<"10 jets "<<nj10[j]<<endl;}
-        else {njmore.push_back(njets); cout<<"more "<< k<<" "<<njmore[j]<<endl;}
+       for(int k=0;k<11;k++) {
+	double njets = plots[j][0].GetBinContent(k);
+        //cout<<k<<" "<<njets<<endl; 
+        if (k == 1)      {nj0.push_back(njets); cout<<"0 jets "<<nj0[j]<<endl;}
+        else if (k == 2) {nj1.push_back(njets); cout<<"1 jets "<<nj1[j]<<endl;}
+        else if (k == 3) {nj2.push_back(njets); cout<<"2 jets "<<nj2[j]<<endl;}
+        else if (k == 4) {nj3.push_back(njets); cout<<"3 jets "<<nj3[j]<<endl;}
+        else if (k == 5) {nj4.push_back(njets); cout<<"4 jets "<<nj4[j]<<endl;}
+        else if (k == 6) {nj5.push_back(njets); cout<<"5 jets "<<nj5[j]<<endl;}
+        else if (k == 7) {nj6.push_back(njets); cout<<"6 jets "<<nj6[j]<<endl;}
+        else if (k == 8) {nj7.push_back(njets); cout<<"7 jets "<<nj7[j]<<endl;}
+        else if (k == 9) {nj8.push_back(njets); cout<<"8 jets "<<nj8[j]<<endl;}
+        else if (k == 10) {nj9.push_back(njets); cout<<"9 jets "<<nj9[j]<<endl;}
+        //else if (k == 11) {nj10.push_back(njets); cout<<"10 jets "<<nj10[j]<<endl;}
+        //else {njmore.push_back(njets); cout<<"more "<< k<<" "<<njmore[j]<<endl;}
        } // close for bins
-       ntot.push_back(nj3[j]+nj4[j]+nj5[j]+nj6[j]+nj7[j]+nj8[j]+nj9[j]+nj10[j]+njmore[j]);
+       ntot.push_back(nj0[j]+nj1[j]+nj2[j]+nj3[j]+
+                      nj4[j]+nj5[j]+nj6[j]+nj7[j]+
+                      nj8[j]+nj9[j]);
        cout<<"total "<<ntot[j]<<endl;
    } // close for masses
+
    TMultiGraph *mg = new TMultiGraph();
+
    int nevents=1;//20000;
    //
    TGraphErrors *TreeJets = new TGraphErrors(1);
@@ -249,9 +265,10 @@ vector<double> nj3,nj4,nj5,nj6,nj7,nj8,nj9,nj10,njmore,ntot;
    TreeJets->SetLineColor(kMagenta);
    TreeJets->SetMarkerColor(kMagenta);
    TreeJets->SetLineWidth(3);
-   for(int j=0;j<maxtodo;j++) TreeJets->SetPoint(j, masses[j], nj3[j]/nevents);
-   //mg->Add(TreeJets,"L,P");//->Draw("L,P");
-   //leg->AddEntry(TreeJets, " 3 jets", "LP");
+   for(int j=0;j<maxtodo;j++) TreeJets->SetPoint(j, masses[j], (nj0[j]+nj1[j]+nj2[j]+nj3[j])/nevents);
+   TreeJets->GetXaxis()->SetTitle("Number of jets before selections");
+   mg->Add(TreeJets,"L,P");//->Draw("L,P");
+   leg->AddEntry(TreeJets, " < 4 jets", "LP");
    //
    TGraphErrors *fourJets = new TGraphErrors(1);
    fourJets->SetMarkerStyle(kFullDotLarge); 
@@ -286,8 +303,8 @@ vector<double> nj3,nj4,nj5,nj6,nj7,nj8,nj9,nj10,njmore,ntot;
    sevenJets->SetMarkerColor(kMagenta);
    sevenJets->SetLineWidth(3);
    for(int j=0;j<maxtodo;j++) sevenJets->SetPoint(j, masses[j], nj7[j]/nevents);
-   mg->Add(sevenJets,"L,P");//->Draw("L,P");
-   leg2->AddEntry(sevenJets, " 7 jets", "LP");
+   //mg->Add(sevenJets,"L,P");//->Draw("L,P");
+   //leg2->AddEntry(sevenJets, " 7 jets", "LP");
    //
    TGraphErrors *eightJets = new TGraphErrors(1);
    eightJets->SetMarkerStyle(kFullDotLarge); 
@@ -295,8 +312,8 @@ vector<double> nj3,nj4,nj5,nj6,nj7,nj8,nj9,nj10,njmore,ntot;
    eightJets->SetMarkerColor(kGreen);
    eightJets->SetLineWidth(3);
    for(int j=0;j<maxtodo;j++) eightJets->SetPoint(j, masses[j], nj8[j]/nevents);
-   mg->Add(eightJets,"L,P");//->Draw("L,P");
-   leg2->AddEntry(eightJets, " 8 jets", "LP");
+   //mg->Add(eightJets,"L,P");//->Draw("L,P");
+   //leg2->AddEntry(eightJets, " 8 jets", "LP");
    //
    TGraphErrors *nineJets = new TGraphErrors(1);
    nineJets->SetMarkerStyle(kFullDotLarge); 
@@ -304,24 +321,15 @@ vector<double> nj3,nj4,nj5,nj6,nj7,nj8,nj9,nj10,njmore,ntot;
    nineJets->SetMarkerColor(50);
    nineJets->SetLineWidth(3);
    for(int j=0;j<maxtodo;j++) nineJets->SetPoint(j, masses[j], nj9[j]/nevents);
-   mg->Add(nineJets,"L,P");//->Draw("L,P");
-   leg2->AddEntry(nineJets, " 9 jets", "LP");
-   //
-   TGraphErrors *tenJets = new TGraphErrors(1);
-   tenJets->SetMarkerStyle(kFullDotLarge); 
-   tenJets->SetLineColor(kGray);
-   tenJets->SetMarkerColor(kGray);
-   tenJets->SetLineWidth(3);
-   for(int j=0;j<maxtodo;j++) tenJets->SetPoint(j, masses[j], nj10[j]/nevents);
-   mg->Add(tenJets,"L,P");//->Draw("L,P");
-   leg2->AddEntry(tenJets, " 10 jets", "LP");
+   //mg->Add(nineJets,"L,P");//->Draw("L,P");
+   //leg2->AddEntry(nineJets, " 9 jets", "LP");
    //
    TGraphErrors *moreJets = new TGraphErrors(1);
    moreJets->SetMarkerStyle(kFullDotLarge); 
    moreJets->SetLineColor(kPink);
    moreJets->SetMarkerColor(kPink);
    moreJets->SetLineWidth(3);
-   for(int j=0;j<maxtodo;j++) moreJets->SetPoint(j, masses[j], njmore[j]/nevents);
+   for(int j=0;j<maxtodo;j++) moreJets->SetPoint(j, masses[j], (nj7[j]+nj8[j]+nj9[j])/nevents);
    mg->Add(moreJets,"L,P");//->Draw("L,P");
    leg2->AddEntry(moreJets, " > 6 jets", "LP");
    //
@@ -335,9 +343,77 @@ vector<double> nj3,nj4,nj5,nj6,nj7,nj8,nj9,nj10,njmore,ntot;
    leg2->AddEntry(totJets, " total", "LP");
    //
   mg->Draw("AP");
+  mg->GetXaxis()->SetTitle("Normalized to Nevents");
+  mg->GetXaxis()->SetTitle("Number of jets before selections");
   leg2->Draw();
   PT_HAT->SetLogy(0);
   PT_HAT->SaveAs("njets_flat.png");
 //PT_HAT->Close(); 
-
+//////////////////////////////////////////////////////////////////////
+// cat plot
+  PT_HAT->Clear();
+  leg2->Clear();
+   TMultiGraph *mg1 = new TMultiGraph();
+   int nevents=1;//20000;
+   vector<double> cat0,cat1,cat2,ctot;
+    for(int j=0;j<maxtodo;j++) {
+       cout<<lege[todo[j]]<<endl;
+       // get number of njets
+       int nbins = plots[j][25].GetNbinsX();
+       //cout<<nbins<<endl;
+       for(int k=0;k<nbins;k++) {
+	double njets = plots[j][25].GetBinContent(k); 
+        if (k == 2)      {cat0.push_back(njets); cout<<"0 tag "<<cat0[j]<<endl;}
+        else if (k == 3) {cat1.push_back(njets); cout<<"1 tag "<<cat1[j]<<endl;}
+        else if (k == 4) {cat2.push_back(njets); cout<<"2 tag "<<cat2[j]<<endl;}
+       } // close for bins
+       ctot.push_back(cat0[j]+cat1[j]+cat2[j]);
+       cout<<"total "<<ctot[j]<<endl;
+   } // close for masses
+   cout<<"sample"<<" "<<"ctot"<<endl;
+   for(int j=0;j<maxtodo;j++) cout<<lege[todo[j]]<<" "<<ctot[j]<<endl;
+   TMultiGraph *mg = new TMultiGraph();
+   //
+   TGraphErrors *cat0Jets = new TGraphErrors(1);
+   cat0Jets->SetMarkerStyle(kFullDotLarge); 
+   cat0Jets->SetLineColor(kMagenta);
+   cat0Jets->SetMarkerColor(kMagenta);
+   cat0Jets->SetLineWidth(3);
+   for(int j=0;j<maxtodo;j++) cat0Jets->SetPoint(j, masses[j], cat0[j]/nevents);
+   mg1->Add(cat0Jets,"L,P");//->Draw("L,P");
+   mg1.SetMaximum(1.25);
+   leg2->AddEntry(cat0Jets, " 0 tag", "LP");
+   //
+   TGraphErrors *cat1Jets = new TGraphErrors(1);
+   cat1Jets->SetMarkerStyle(kFullDotLarge); 
+   cat1Jets->SetLineColor(8);
+   cat1Jets->SetMarkerColor(8);
+   cat1Jets->SetLineWidth(3);
+   for(int j=0;j<maxtodo;j++) cat1Jets->SetPoint(j, masses[j], cat1[j]/nevents);
+   mg1->Add(cat1Jets,"L,P");//->Draw("L,P");
+   leg2->AddEntry(cat1Jets, " 1 tag", "LP");
+   //
+   TGraphErrors *cat2Jets = new TGraphErrors(1);
+   cat2Jets->SetMarkerStyle(kFullDotLarge); 
+   cat2Jets->SetLineColor(kCyan);
+   cat2Jets->SetMarkerColor(kCyan);
+   cat2Jets->SetLineWidth(3);
+   for(int j=0;j<maxtodo;j++) cat2Jets->SetPoint(j, masses[j], cat2[j]/nevents);
+   mg1->Add(cat2Jets,"L,P");//->Draw("L,P");
+   leg2->AddEntry(cat2Jets, " 2 tags", "LP");
+   //
+   TGraphErrors *totJets = new TGraphErrors(1);
+   totJets->SetMarkerStyle(kFullDotLarge); 
+   totJets->SetLineColor(1);
+   totJets->SetMarkerColor(1);
+   totJets->SetLineWidth(3);
+   for(int j=0;j<maxtodo;j++) totJets->SetPoint(j, masses[j], ctot[j]/nevents);
+   mg1->Add(totJets,"L,P");//->Draw("L,P");
+   leg2->AddEntry(totJets, " total", "LP");
+   //
+  mg1->Draw("AP");
+  mg1->GetXaxis()->SetTitle("Final efficiency");
+  leg2->Draw();
+  PT_HAT->SetLogy(0);
+  PT_HAT->SaveAs("fatcat.png");
 }

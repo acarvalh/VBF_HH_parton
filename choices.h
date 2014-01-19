f9697fa84552e7828095e029a215121fae0eaf65
Xanda <xanda@xanda-ThinkPad-X220-Tablet.(none)>
2014-01-19 19:37:54 -0200

    full substructure

--- choices.h
+++ choices.h
@@ -1,10 +1,10 @@
 // inputs
-unsigned const nevent=20000;
+int const nevent=50000;
 // Analysis
 bool fourb=true;
 bool resonant=false;
 // for nonresonant
-unsigned const parameters=9;
+int const parameters=9;
 int parameter[parameters]={
 1001,1000,101010,
 51010,151010,
@@ -26,10 +26,15 @@ string filenames[parameters]={
 "BSM_13tev_VBFcuts_CV_p1p0_C2V_p1p0_C3_p2p0"
 };
 // for resonant
-unsigned const masses=11;
-int mass[masses]={260,300,350,400,450,
-                  500,550,600,650,700,750};
+int const masses=26;
+int mass[masses]={260,300 ,350 ,400 ,450 ,500 ,
+		      550 ,600 ,650 ,700 ,750,
+		      800 ,850 ,900 ,950 ,1000,
+                      1050,1100,1150,1200,1250,
+		      1300,1350,1400,1450,1500
+                 };
 
-
+//unsigned const masses=2;
+//int mass[masses]={260,500};
 /////////////////////////////////////
 
